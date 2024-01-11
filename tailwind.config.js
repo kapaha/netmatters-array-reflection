a91407f9cdeb4b18667a8c7f2d016c292bbabb2b
Kai Paterson-Hall <kaizwebdesign@gmail.com>
2024-01-11 14:15:44 +0000

    Include javascript file paths in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.html"],
+    content: ["./index.html", "./index.js", "./modules/**/*.js"],
     theme: {
         extend: {
             height: {
